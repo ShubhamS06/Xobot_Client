4011aee49f749c9b03723385d0cbd1876d543023
Praveen Yadav <praveenyadav7787@gmail.com>
2024-07-28 01:13:12 +0530

    fix click animation position in what our client

--- Components/WhatOurClientsAreSayingSection/index.jsx
+++ Components/WhatOurClientsAreSayingSection/index.jsx
@@ -21,7 +21,7 @@ function WhatOurClientsAreSayingSection({ styles }) {
             </div>
             <div className={styles.whatOurCardGroup}>
                 {cards/* .slice(0, 1) */.map((card, index) => (
-                    <div key={index} onClick={() => setActiveCard(index)} data-dir={activeCard == index ? '' : (activeCard > index ? 'left' : 'right')} data-active={activeCard == index} className={styles.whatOurCard}>
+                    <div key={index} onClick={() => setActiveCard(index)} data-dir={index - activeCard} data-active={activeCard == index} className={styles.whatOurCard}>
                         <div className={styles.whatOurClientCardBody}>
                             <div className={styles.selfServeCustomerSupportParent}>
                                 <b className={`${styles.sectionMainHeading} ${styles.whatOurCardTitle}`}>
