417563d4c286a05a46894548fe0e4c0e29652210
Praveen Yadav <praveenyadav7787@gmail.com>
2024-07-28 21:33:58 +0530

    fix maximize padding and gredient

--- Components/MaximizeSection/index.jsx
+++ Components/MaximizeSection/index.jsx
@@ -1,50 +1,47 @@
-import React from 'react'
+import React from "react";
 import compStyles from "./maximize.module.css";
 
 function MaximizeSection({ styles }) {
-    return (
-        <div className={compStyles.maximizeSection}>
-            <img className={compStyles.maximizeHeroStars} src="/hero-stars.svg" alt="" />
-            <img className={compStyles.maximizeHeroStars2} src="/hero-stars.svg" alt="" />
+  return (
+    <div className={compStyles.maximizeSection}>
+      <img
+        className={compStyles.maximizeHeroStars}
+        src="/hero-stars.svg"
+        alt=""
+      />
+      <img
+        className={compStyles.maximizeHeroStars2}
+        src="/hero-stars.svg"
+        alt=""
+      />
 
-            <div className={compStyles.maximizeSectionChild} />
-            <div className={compStyles.frameParent5}>
-                <div className={compStyles.maximizeYourSupportCapacityParent}>
-                    <b className={compStyles.maximizeYourSupport}>
-                        Maximize your support capacity without extra hiring
-                    </b>
-                    <div className={compStyles.spend5Minutes}>
-                        Spend 5 minutes to get a ChatGPT powered chatbot that works 24/7
-                        to help you engage and retain more users and boost conversions.
-                    </div>
-                </div>
-                <div className={compStyles.frameParent}>
-                    <button className={`${styles.getStartedForFreeWrapper} ${styles.getStartedFor}`}>
-                        Get started for free
-                    </button>
-                    <button className={`${styles.getStartedForFreeWrapper} ${styles.vectorParent}`}>
-                        <img
-                            className={styles.frameChild}
-                            alt=""
-                            src="/polygon-2.svg"
-                        /> Watch Video
-                    </button>
-                    {/* <div className={styles.buttonInner}>
-                        <div className={styles.vectorParent}>
-                            <img
-                                className={styles.frameChild}
-                                alt=""
-                                src="/polygon-2.svg"
-                            />
-                            <div className={styles.getStartedFor}>Watch Video</div>
-                        </div>
-                    </div> */}
-                </div>
-                {/* <img className={compStyles.unionIcon8} alt="" src="/union3.svg" />
-                <img className={compStyles.unionIcon8} alt="" src="/union3.svg" /> */}
-            </div>
+      <div className={compStyles.maximizeSectionChild} />
+      <div className={compStyles.frameParent5}>
+        <div className={compStyles.maximizeYourSupportCapacityParent}>
+          <b className={compStyles.maximizeYourSupport}>
+            Maximize your support capacity without extra hiring
+          </b>
+          <div className={compStyles.spend5Minutes}>
+            Spend 5 minutes to get a ChatGPT powered chatbot that works 24/7 to
+            help you engage and retain more users and boost conversions.
+          </div>
         </div>
-    )
+        <div className={compStyles.frameParent}>
+          <button
+            className={`${styles.getStartedForFreeWrapper} ${styles.getStartedFor}`}
+          >
+            Get started for free
+          </button>
+          <button
+            className={`${styles.getStartedForFreeWrapper} ${styles.vectorParent}`}
+          >
+            <img className={styles.frameChild} alt="" src="/polygon-2.svg" />{" "}
+            Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MaximizeSection
+export default MaximizeSection;
