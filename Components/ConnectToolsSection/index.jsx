8011143a64c04baa620c8241b1f05bc2c7d4dc41
Shubham Sahu <sahushubham7342@gmail.com>
2024-07-30 17:17:49 +0530

    removed comments

--- Components/ConnectToolsSection/index.jsx
+++ Components/ConnectToolsSection/index.jsx
@@ -5,7 +5,6 @@ function ConnectToolsSection({ styles }) {
     return (
         <div className={compStyles.connectParent}>
             <div className={compStyles.rectangleParent21}>
-                {/* <div className={compStyles.groupChild52} /> */}
                 <div className={compStyles.connectToToolsYouUseParent}>
                     <b className={styles.sectionMainHeading}>
                         Connect to tools you use
@@ -16,25 +15,6 @@ function ConnectToolsSection({ styles }) {
                 </div>
 
                 <img className={compStyles.toolImage} src='/connect-tools.svg' alt="" />
-
-                {/* <div className={compStyles.stack}>
-                    <div className={compStyles.frameParent15}>
-                        <img src="/connect-tools-images.svg" alt="" />
-                    </div>
-                    <div className={compStyles.unionParent9}>
-                        <div className={compStyles.union6}>
-                            <img className={compStyles.unionChild10} src='/connect-tools.svg' alt="" />
-                            <img className={compStyles.unionIcon20} alt="" src="/union7.svg" />
-                        </div>
-                        <div className={compStyles.rectangleParent22}>
-                            <div className={compStyles.groupChild53} />
-                            <div className={compStyles.groupChild54} />
-                            <div className={compStyles.groupChild55} />
-                            <div className={compStyles.groupChild56} />
-                            <div className={compStyles.groupChild57} />
-                        </div>
-                    </div>
-                </div> */}
             </div>
         </div>
     )
--- Components/FaqSection/index.jsx
+++ Components/FaqSection/index.jsx
@@ -19,7 +19,6 @@ function FaqSection({ styles }) {
     return (
         <div className={styleModule.faqSection}>
             <b className={styleModule.sectionMainHeading}>FAQ's</b>
-            {/* <div className={styles.unionParent4}> */}
             <div className={styleModule.faqQuestionGroup}>
                 {faqItems.map((faq, index) => (
                     <CommonAccordion key={index} index={index} question={faq.question} answar={"test accordian ans"} onExpand={onExpand} isExpanded={expandedIndex === index} />
@@ -30,48 +29,3 @@ function FaqSection({ styles }) {
 }
 
 export default FaqSection;
-
-
-
-
-// import React from 'react'
-
-// function FaqSection({ styles }) {
-//     return (
-//         <div className={styles.faqSection}>
-//             <b className={styles.sectionMainHeading}>FAQ’s</b>
-//             {<div className={styles.unionParent4}>
-//                 <img className={styles.unionIcon10} alt="" src="/union4.svg" />
-//                 <div className={styles.frameParent7}>
-//                     <div className={styles.parent}>
-//                         <div className={styles.div}>01</div>
-//                         <div className={styles.div}>What is Xobot?</div>
-//                     </div>
-//                     <img className={styles.unionIcon11} alt="" src="/union5.svg" />
-//                 </div>
-//                 <div className={styles.div1}>02</div>
-//                 <div className={styles.doesItSupport}>
-//                     Does it support all languages?
-//                 </div>
-//                 <img className={styles.unionIcon12} alt="" src="/union5.svg" />
-//                 <div className={styles.div2}>03</div>
-//                 <div className={styles.doINeed}>
-//                     Do I need to know code to use Xobot?
-//                 </div>
-//                 <img className={styles.unionIcon12} alt="" src="/union5.svg" />
-//                 <div className={styles.div3}>04</div>
-//                 <div className={styles.willIBe}>
-//                     Will I be able to embed the chatbot into my website?
-//                 </div>
-//                 <img className={styles.unionIcon12} alt="" src="/union5.svg" />
-//                 <div className={styles.div4}>05</div>
-//                 <div className={styles.canMultipleTeam}>
-//                     Can multiple team members in my organization manage my chatbots?
-//                 </div>
-//                 <img className={styles.unionIcon12} alt="" src="/union5.svg" />
-//             </div>}
-//         </div>
-//     )
-// }
-
-// export default FaqSection
