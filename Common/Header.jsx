b9235ddeeaaddbfe06c69a84b55c2e1b4ebe1bf3
Praveen Yadav <praveenyadav7787@gmail.com>
2024-07-31 23:43:16 +0530

    feat: separate header content for below 800px and animate

--- Common/Header.jsx
+++ Common/Header.jsx
@@ -4,31 +4,36 @@ function Header({ styles }) {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className={styles.header}>
+    <div className={styles.header} data-show={showMenu}>
       <div className={styles.headerInner}>
         <img className={styles.headerBrandIcon} alt="" src="/group-15@2x.png" />
       </div>
-      <div onClick={() => setShowMenu(!showMenu)} className={styles.togglerRoot} data-show={showMenu}>
+      <div
+        onClick={() => setShowMenu(!showMenu)}
+        className={styles.togglerRoot}
+      >
         <button className={styles.menuToggler}></button>
       </div>
 
-      <div className={styles.headerMenu} data-show={showMenu}>
-        <li className={styles.menuItem}>For Business</li>
-        <li className={styles.menuItem}>Pricing</li>
-        <li className={styles.menuItem}>Contact</li>
-        <li className={styles.menuItem}>Blogs</li>
-      </div>
-      <div className={styles.headerAuthGroup} data-show={showMenu}>
-        <button
-          className={`${styles.signUpWrapper} ${styles.footerLinksHeader}`}
-        >
-          Sign In
-        </button>
-        <button
-          className={`${styles.signUpWrapper} ${styles.footerLinksHeader}`}
-        >
-          Sign Up
-        </button>
+      <div className={styles.headerContent}>
+        <div className={styles.headerMenu}>
+          <li className={styles.menuItem}>For Business</li>
+          <li className={styles.menuItem}>Pricing</li>
+          <li className={styles.menuItem}>Contact</li>
+          <li className={styles.menuItem}>Blogs</li>
+        </div>
+        <div className={styles.headerAuthGroup}>
+          <button
+            className={`${styles.signUpWrapper} ${styles.footerLinksHeader}`}
+          >
+            Sign In
+          </button>
+          <button
+            className={`${styles.signUpWrapper} ${styles.footerLinksHeader}`}
+          >
+            Sign Up
+          </button>
+        </div>
       </div>
     </div>
   );
