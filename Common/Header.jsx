4a4b6892aba3a9b669b0c0d9f05dd62a65831827
Praveen Yadav <praveenyadav7787@gmail.com>
2024-07-31 22:44:23 +0530

    feat: add header drop down menu

--- Common/Header.jsx
+++ Common/Header.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Header({ styles }) {
-    return (
-        <div className={styles.header}>
-            <div className={styles.headerInner}>
-                <img className={styles.headerBrandIcon} alt="" src="/group-15@2x.png" />
-            </div>
-            <div className={styles.togglerRoot}>
-                <button className={styles.menuToggler}></button>
-            </div>
+  const [showMenu, setShowMenu] = useState(false);
 
-            <div className={styles.headerMenu}>
-                <li className={styles.menuItem}>For Business</li>
-                <li className={styles.menuItem}>Pricing</li>
-                <li className={styles.menuItem}>Contact</li>
-                <li className={styles.menuItem}>Blogs</li>
-            </div>
-            <div className={styles.headerAuthGroup}>
-                <button className={`${styles.signUpWrapper} ${styles.footerLinksHeader}`}>Sign In</button>
-                <button className={`${styles.signUpWrapper} ${styles.footerLinksHeader}`}>Sign Up</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerInner}>
+        <img className={styles.headerBrandIcon} alt="" src="/group-15@2x.png" />
+      </div>
+      <div onClick={() => setShowMenu(!showMenu)} className={styles.togglerRoot}>
+        <button className={styles.menuToggler}></button>
+      </div>
+
+      <div className={styles.headerMenu} data-show={showMenu}>
+        <li className={styles.menuItem}>For Business</li>
+        <li className={styles.menuItem}>Pricing</li>
+        <li className={styles.menuItem}>Contact</li>
+        <li className={styles.menuItem}>Blogs</li>
+      </div>
+      <div className={styles.headerAuthGroup} data-show={showMenu}>
+        <button
+          className={`${styles.signUpWrapper} ${styles.footerLinksHeader}`}
+        >
+          Sign In
+        </button>
+        <button
+          className={`${styles.signUpWrapper} ${styles.footerLinksHeader}`}
+        >
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
